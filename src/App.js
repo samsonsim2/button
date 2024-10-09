1afcfb122c8433383e7f389c5bede7122f66a633
Samsonsim2 <samsonsim2@gmail.com>
2024-10-09 12:56:41 +0800

    button

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React, { useEffect, useState } from 'react';
+import { motion, useAnimate } from 'framer-motion';
+function App() {
+
+  const [scope, animate] = useAnimate();
+  const [isVisible, setIsVisible] = useState(true);
+  const playSound = () => {
+    const audio = new Audio('/audio/button04.mp3'); // Replace with your sound file path
+    audio.volume = 0.3; 
+    audio.play();
+  };
+
+  const handleClick = () => {
+    // Move down by 20px
+    console.log("test")
+    animate(".dial", { y: 10 }, { type: 'tween', stiffness: 300 })
+      .then(() => {
+        // Bring it back to the original position
+        animate(".dial", { y: 0 }, { type: 'tween', stiffness: 300 });
+      });
+
+      animate(".dialShadow", { opacity: 0.3 }, { type: 'tween', stiffness: 300 })
+      .then(() => {
+        // Bring it back to the original position
+        animate(".dialShadow", { opacity: 0.2 }, { type: 'tween', stiffness: 300 });
+      });
+      setIsVisible(!isVisible);
+
+      playSound()
+     
+
+  };
+
+ 
+
+
+ 
+  return (
+    <main className="main"  ref={scope}>
+      <div className="white">
+      <motion.div 
+ // Handle click event
+       initial={{ opacity: 1 }} // Initial position
+       animate={{ opacity: isVisible ? 1 : 0 }}
+       transition={{ duration: 0.8 }} // Animate to the new Y position
+       className='orange' > 
+        
+        </motion.div>
+   
+        </div>
+      <div className='dialGroup' >
+   
+        <motion.div
+         
+          onClick={handleClick} // Handle click event
+
+          className="dial">
+          <div className="highlight">
+
+          </div>
+
+        </motion.div>
+        <div className="base">
+        </div>
+
+        <div className="hole">
+        </div>
+
+        <div className="dialShadow">
+        </div>
+
+      </div>
+    </main>
+
+  );
+}
+
+export default App;
